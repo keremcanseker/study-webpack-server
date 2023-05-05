6b265ef20d49fec7a6db1f179f920a0a6702bc0b
keremcanskr <kerem.4555@gmail.com>
2023-05-06 00:43:27 +0300

    add a employee

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { Request } from "./requests";
-
+import { UI } from "./ui";
 
 const form = document.getElementById("employee-form");
 const nameInput = document.getElementById("name");
@@ -11,6 +11,7 @@ const updateEmployeeButton = document.getElementById("update");
 const request = new Request("http://localhost:3000/employees");
 
 // GET REQUEST
+
 // request.get().then(employees => console.log(employees)).catch(err => console.log(err))
 
 // POST REQUEST
@@ -19,6 +20,7 @@ const request = new Request("http://localhost:3000/employees");
 //     .catch(err => console.log(err));
 
 //PUT REQUEST
+
 // request.put(1, { name: "ahmet", department: "advertise", salary: 2000 })
 //     .then(data => console.log(data))
 //     .catch(err => console.log(err));
@@ -26,7 +28,41 @@ const request = new Request("http://localhost:3000/employees");
 
 // DELETE REQUEST
 
-request.delete(2)
-    .then(message => console.log(message))
-    .catch(err => console.log(err));
+// request.delete(2)
+//     .then(message => console.log(message))
+//     .catch(err => console.log(err));
 
+const ui = new UI();
+
+eventListeners();
+
+function eventListeners() {
+    document.addEventListener("DOMContentLoaded", getAllEmployees);
+    form.addEventListener("submit", addEmployee)
+}
+
+function getAllEmployees() {
+    request.get()
+        .then(data => {
+            ui.addAllEmployeesToUI(data);
+        })
+        .catch(err => console.log(err));
+}
+
+function addEmployee(e) {
+    const employeeName = nameInput.value.trim();
+    const employeeDepartment = departmentInput.value.trim();
+    const employeeSalary = salaryInput.value.trim();
+
+    if (employeeName === "" || employeeDepartment === "" || employeeSalary === "") {
+        alert("please fill everything");
+    } else {
+        request.post({ name: employeeName, department: employeeDepartment, salary: Number.employeeSalary })
+            .then(employee => {
+                ui.addEmployeeToUI(employee)
+            }).catch(err => console.log(err));
+    }
+
+    ui.clearInputs();
+    e.preventDefault();
+}
