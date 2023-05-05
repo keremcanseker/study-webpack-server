b4f8bf0087288e4ba2c21926546cc39f15131aad
keremcanskr <kerem.4555@gmail.com>
2023-05-05 21:07:24 +0300

    post request

--- src/index.js
+++ src/index.js
@@ -9,4 +9,9 @@ const employeesList = document.getElementById("employees");
 const updateEmployeeButton = document.getElementById("update");
 
 const request = new Request("http://localhost:3000/employees");
-request.get().then(employees => console.log(employees)).catch(err => console.log(err))
+// request.get().then(employees => console.log(employees)).catch(err => console.log(err))
+
+
+request.post({ name: "ali", department: "management", salary: 5000 })
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
--- src/requests.js
+++ src/requests.js
@@ -10,4 +10,16 @@ export class Request {
         return responseData;
 
     }
+
+    async post(data) {
+        const response = await fetch(this.url, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        });
+        const responseData = await response.json();
+        return responseData;
+    }
 }
