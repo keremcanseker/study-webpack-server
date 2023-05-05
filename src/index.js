bcacc124800807d80190f45a3866d28ac2a28f64
keremcanskr <kerem.4555@gmail.com>
2023-05-05 21:00:05 +0300

    get request

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Test");
+import { Request } from "./requests";
+
+
+const form = document.getElementById("employee-form");
+const nameInput = document.getElementById("name");
+const departmentInput = document.getElementById("department")
+const salaryInput = document.getElementById("salary");
+const employeesList = document.getElementById("employees");
+const updateEmployeeButton = document.getElementById("update");
+
+const request = new Request("http://localhost:3000/employees");
+request.get().then(employees => console.log(employees)).catch(err => console.log(err))
